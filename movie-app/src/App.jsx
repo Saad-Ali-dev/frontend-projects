5390e04bd5701e3e51b90f78b981f72fe95e0a16
Saad Ali <a.saadmughal57@gmail.com>
2025-04-04 15:26:42 +0500

    Initial Navbar completed

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,10 +1,11 @@
+import Navbar from "./components/Navbar.jsx";
 
 
 function App() {
 
   return (
     <>
-      
+      <Navbar />
     </>
   )
 }
---
+++ movie-app/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+export default function Navbar() {
+  return (
+    <header>
+      <nav className="bg-white/20 rounded-2xl shadow-[0_4px_30px_rgba(0,0,0,0.1)] backdrop-blur-sm border border-white/30 p-4 mx-4 my-2">
+        <div className="container mx-auto flex items-center justify-center gap-8">
+          <a
+            href="/"
+            className="text-base md:text-lg font-semibold hover:text-gray-500 transition-colors"
+          >
+            Home
+          </a>
+          <a
+            href="/movies"
+            className="text-base md:text-lg font-semibold hover:text-gray-500 transition-colors"
+          >
+            Movies
+          </a>
+          <a
+            href="/tv"
+            className="text-base md:text-lg font-semibold hover:text-gray-500 transition-colors"
+          >
+            TV Shows
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+}
